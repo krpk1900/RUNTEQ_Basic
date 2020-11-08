18b5a887912b471ab58d4b4675028e585bdb458c
krpk1900 <krpk1900@gmail.com>
2020-11-09 07:40:10 +0900

    [Add] 新規登録の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    binding.pry
+    if @user.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :email, :password, :password_confirmation)
   end
 end
