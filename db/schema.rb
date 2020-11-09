c2c243ed115eacab5c0fbe911cae070b67ed7f13
krpk1900 <krpk1900@gmail.com>
2020-11-09 11:23:21 +0900

    [Add] データベースにNOT NULL制約を付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_021759) do
+ActiveRecord::Schema.define(version: 2020_11_09_022211) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_09_021759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "last_name"
-    t.string "first_name"
+    t.string "first_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
