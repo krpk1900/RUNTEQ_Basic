1127334be5b5dd8d5caef327088e3e5697711b1a
krpk1900 <krpk1900@gmail.com>
2020-11-10 23:58:13 +0900

    [Add] boardモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_022603) do
+ActiveRecord::Schema.define(version: 2020_11_10_145239) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
