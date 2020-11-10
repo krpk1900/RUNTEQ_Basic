743fdc6fdba824d3927e7e64b2f1ac8f33eccef7
krpk1900 <krpk1900@gmail.com>
2020-11-11 00:11:33 +0900

    [Add] データベースに文字数制限を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_145239) do
+ActiveRecord::Schema.define(version: 2020_11_10_150724) do
 
   create_table "boards", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "body", null: false
+    t.string "title", limit: 255, null: false
+    t.text "body", limit: 65535, null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
