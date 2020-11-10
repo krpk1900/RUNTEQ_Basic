9e06ab25fbe64718677f1df20d744ea83333e830
krpk1900 <krpk1900@gmail.com>
2020-11-11 04:18:01 +0900

    [Add] board.indexのビューを作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,9 @@
 class BoardsController < ApplicationController
-  def new
-  end
+  def new; end
 
-  def show
-  end
+  def show; end
 
   def index
+    @boards = Board.all.includes(:user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :boards
+  has_many :boards, dependent: :destroy
   authenticates_with_sorcery!
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
