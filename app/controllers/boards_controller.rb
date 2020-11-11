9481bc82b9ab83abc40bbedbe34d0a2c29c3a612
krpk1900 <krpk1900@gmail.com>
2020-11-12 00:37:05 +0900

    [Add] 掲示板作成機能の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,28 @@
 class BoardsController < ApplicationController
-  def new; end
+  skip_before_action :require_login
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to boards_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :new
+    end
+  end
 
   def show; end
 
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body)
   end
 end
