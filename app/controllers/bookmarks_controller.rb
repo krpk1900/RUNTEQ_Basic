8415c788ca2108cc1d5387f676d5f733c543354a
krpk1900 <krpk1900@gmail.com>
2020-11-17 13:20:44 +0900

    [Remove] フラッシュメッセージの削除

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,14 +2,12 @@ class BookmarksController < ApplicationController
   def create
     @board = Board.find(params[:board_id])
     current_user.bookmark(@board)
-    flash.now[:success] = t('.success')
     # redirect_back fallback_location: root_path, success: t('.success')
   end
 
   def destroy
     @board = current_user.bookmarks.find(params[:id]).board
     current_user.unbookmark(@board)
-    flash.now[:success] = t('.success')
     # redirect_back fallback_location: root_path, success: t('.success')
   end
 end
