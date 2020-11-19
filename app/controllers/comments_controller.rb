51bcc4965ea3fd2a1512bc18202665608b47deed
krpk1900 <krpk1900@gmail.com>
2020-11-19 18:21:02 +0900

    [Modify] リファクタリング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,24 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[destroy update]
-
   def create
     @comment = current_user.comments.build(comment_params)
     @comment.save
   end
 
   def destroy
+    @comment = current_user.comments.find(params[:id])
     @comment.destroy!
   end
 
   def update
+    @comment = current_user.comment.find(params[:id])
     if @comment.update(comment_update_params)
-      render json: @comment
+      render json: { comment: @comment }, status: :ok
     else
-      head :bad_request
+      render json: { comment: @comment, errors: { messages: @comment.errors.full_messages } }, status: :bad_request
     end
   end
 
   private
-
-  def set_comment
-    @comment = Comment.find_by(id: params[:id])
-  end
 
   def comment_params
     params.require(:comment).permit(:body).merge(board_id: params[:board_id])
