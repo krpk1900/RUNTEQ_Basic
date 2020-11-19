13c85f1973e473c364084750d54f9de4768bc14d
krpk1900 <krpk1900@gmail.com>
2020-11-19 22:00:30 +0900

    [Add] ブックマーク一覧もページネーションを実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -44,7 +44,7 @@ class BoardsController < ApplicationController
   end
 
   def bookmarks
-    @bookmark_boards = current_user.bookmark_boards.includes(:user).order(created_at: :desc)
+    @bookmark_boards = current_user.bookmark_boards.includes(:user).order(created_at: :desc).page(params[:page]).per(20)
   end
 
   private
