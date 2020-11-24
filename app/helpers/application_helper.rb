0ad03e48e522c15e376220a6f8d7bca021e81f23
krpk1900 <krpk1900@gmail.com>
2020-11-24 23:27:53 +0900

    [Modify] コーディング規約に沿うように修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def page_title(page_title = '', admin = false)
     base_title = if admin
-      'RUNTEQ BOARD APP(管理画面)'
-    else
-      'RUNTEQ BOARD APP'
-    end
+                   'RUNTEQ BOARD APP(管理画面)'
+                 else
+                   'RUNTEQ BOARD APP'
+                 end
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
 end
