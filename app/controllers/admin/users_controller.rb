08ebd3f8465a6af841458dfa83d1eea71ee69659
krpk1900 <krpk1900@gmail.com>
2020-11-28 01:10:06 +0900

    [Add] ユーザー編集ページの作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,22 +1,31 @@
 class Admin::UsersController < Admin::BaseController
 
   def index
-    @users = User.all
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true).order(created_at: :desc)
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
 
+    @user = User.find(params[:id])
+    @user.update(user_params)
   end
 
   def destroy
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :email, :avatar, :role)
   end
 end
