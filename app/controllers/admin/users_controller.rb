44cc11bd8f0f0db55adb1615e4ceb2e4edb926e5
krpk1900 <krpk1900@gmail.com>
2020-11-28 01:26:04 +0900

    [Modify] ユーザーコントローラをリファクタリング

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,29 +1,34 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: %i[show edit update destroy]
 
   def index
     @q = User.ransack(params[:q])
     @users = @q.result(distinct: true).order(created_at: :desc)
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-
-    @user = User.find(params[:id])
-    @user.update(user_params)
+    if @user.update(user_params)
+      redirect_to admin_users_path, success: t('defaults.message.updated', item: User.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: User.model_name.human)
+      render edit_admin_user_path
+    end
   end
 
   def destroy
-
+    @user.destroy!
+    redirect_to admin_users_path, success: t('defaults.message.deleted', item: User.model_name.human)
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:last_name, :first_name, :email, :avatar, :role)
