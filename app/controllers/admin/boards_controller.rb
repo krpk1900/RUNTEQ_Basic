3e23ce0975a8f369572f9025e34ebbdc7c70e418
krpk1900 <krpk1900@gmail.com>
2020-11-28 01:48:52 +0900

    [Add] Admin::boardsコントローラを作成

---
+++ app/controllers/admin/boards_controller.rb
@@ -0,0 +1,6 @@
+class Admin::BoardsController < Admin::BaseController
+  def index
+    @q = Board.ransack(params[:q])
+    @boards = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
+  end
+end
