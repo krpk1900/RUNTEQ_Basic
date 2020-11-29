91315972d69d811259b221ae151d35800fbec34b
krpk1900 <krpk1900@gmail.com>
2020-11-30 04:11:15 +0900

    [Add] サイドバーのアクティブ/非アクティブ化を実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,7 +5,7 @@ class Admin::UsersController < Admin::BaseController
     @q = User.ransack(params[:q])
     @users = @q.result(distinct: true).order(created_at: :desc)
 
-    binding.pry
+    # binding.pry
 
   end
 
@@ -14,8 +14,7 @@ class Admin::UsersController < Admin::BaseController
   def edit; end
 
   def update
-    if @user.update(user_params)@q
-
+    if @user.update(user_params)
       redirect_to admin_users_path, success: t('defaults.message.updated', item: User.model_name.human)
     else
       flash.now[:danger] = t('defaults.message.not_updated', item: User.model_name.human)
